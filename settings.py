072440cad40a49fdf189627af177b95a683524ce
Abdur-RahmaanJ <cryptolabour@gmail.com>
2023-08-29 19:41:14 +0400

    chore: rename to docs

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 from jamstack.api.template import load_json
 import os 
 
-OUTPUT_FOLDER = 'dist/'
+OUTPUT_FOLDER = 'docs/'
 DATA_PATH = 'data/'
 POSTS_PATH = os.path.join(DATA_PATH, "posts")
 AUTHORS_PATH = os.path.join(DATA_PATH, "authors")
